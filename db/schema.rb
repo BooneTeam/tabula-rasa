0d0d709373b411b134e9e995c2e9083437f9ddab
Garrett Boone <Boone.Garrett@gmail.com>
2016-11-23 17:53:41 -0600

    switch to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161123164422) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
     t.text     "body"
@@ -21,9 +24,9 @@ ActiveRecord::Schema.define(version: 20161123164422) do
     t.integer  "author_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
-    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
   end
 
   create_table "admin_users", force: :cascade do |t|
@@ -39,8 +42,8 @@ ActiveRecord::Schema.define(version: 20161123164422) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_admin_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
   end
 
   create_table "apps", force: :cascade do |t|
@@ -54,7 +57,7 @@ ActiveRecord::Schema.define(version: 20161123164422) do
     t.string  "homepage"
     t.string  "version"
     t.integer "category_id"
-    t.index ["category_id"], name: "index_apps_on_category_id"
+    t.index ["category_id"], name: "index_apps_on_category_id", using: :btree
   end
 
   create_table "basic_users", force: :cascade do |t|
@@ -70,8 +73,8 @@ ActiveRecord::Schema.define(version: 20161123164422) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_basic_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_basic_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_basic_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_basic_users_on_reset_password_token", unique: true, using: :btree
   end
 
   create_table "categories", force: :cascade do |t|
@@ -83,8 +86,8 @@ ActiveRecord::Schema.define(version: 20161123164422) do
     t.integer  "app_id"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
-    t.index ["app_id"], name: "index_system_config_apps_on_app_id"
-    t.index ["system_config_id"], name: "index_system_config_apps_on_system_config_id"
+    t.index ["app_id"], name: "index_system_config_apps_on_app_id", using: :btree
+    t.index ["system_config_id"], name: "index_system_config_apps_on_system_config_id", using: :btree
   end
 
   create_table "system_configs", force: :cascade do |t|
@@ -92,7 +95,7 @@ ActiveRecord::Schema.define(version: 20161123164422) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_system_configs_on_user_id"
+    t.index ["user_id"], name: "index_system_configs_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
